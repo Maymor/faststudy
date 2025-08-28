089075266e4be2d2a7e47dbc2a6659a3122acc18
Maymor <dimalitvinov1909@gmail.com>
2025-08-28 20:00:20 +0300

    Added new task, clear config and delete test tasks

--- src/Modules/Config/Config.php
+++ src/Modules/Config/Config.php
@@ -11,11 +11,4 @@ class Config
         $this->base_path = dirname($_SERVER['DOCUMENT_ROOT']);
         $this->config_path = $this->base_path . '/configs';
     }
-
-    public function printPath(): void
-    {
-        echo $this->config_path;
-        echo "\n";
-        echo $this->base_path;
-    }
 }
